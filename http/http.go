1c7a39f754bf04f9e257dd2e378a66046501e5fc
Hyeon Soo Yoo <82surf@gmail.com>
2024-05-29 17:34:39 +0900

    [feat] http client에 tls 적용

--- http/http.go
+++ http/http.go
@@ -2,13 +2,41 @@ package http
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"io"
 	"k8s.io/klog/v2"
 	"net/http"
+	"os"
 )
 
+var client *http.Client
+
+func init() {
+	initHttpClient()
+}
+
+func initHttpClient() {
+	// Load CA cert
+	caCert, err := os.ReadFile(os.Getenv("CA_CERT_PATH"))
+	if err != nil {
+		klog.V(1).Infoln(err)
+		return
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup Https client
+	tlsConfig := &tls.Config{
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: false,
+	}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client = &http.Client{Transport: transport}
+}
+
 func Post(jsonData []byte) {
-	url := "http://localhost:38080/post"
+	url := os.Getenv("HC_API_SERVER_URL")
 	request, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
 	if err != nil {
 		klog.V(1).Infoln(err)
@@ -16,7 +44,6 @@ func Post(jsonData []byte) {
 	}
 	request.Header.Set("Content-Type", "application/json")
 
-	client := http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
 		klog.V(1).Infoln(err)
